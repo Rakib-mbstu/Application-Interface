5701f8cb37203805d72bd22e96ee4047e85e3153
rakib-mbstu <rakibshanto41@gmail.com>
2024-06-12 13:06:48 +0600

    feat: middleware added

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const initCall = require("./server/crypto/connector");
 const User = require("./server/models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cookie = require("cookie");
 const jwtSecret = process.env.jwtSecret;
 
 //
@@ -25,6 +27,22 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+//middleware
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
 // Route for the home page
 app.get("/", async (req, res) => {
@@ -33,8 +51,8 @@ app.get("/", async (req, res) => {
 });
 app.post("/login", async (req, res) => {
   try {
-    const { userName, password } = req.body;
-    const user = await User.findOne({ userName });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
     if (!user) {
       return res.status(401).json({ message: "Invalid" });
     }
@@ -49,12 +67,39 @@ app.post("/login", async (req, res) => {
     console.log(error);
   }
 });
+// app.post("/register", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     console.log(password);
+//     const hashPassword = await bcrypt.hash(password, 10);
+//     try {
+//       const user = await User.create({
+//         username: username,
+//         password: hashPassword,
+//       });
+//       console.log(user);
+//       res.status(201).json({ message: "user created" });
+//     } catch (error) {
+//       console.log(error);
+//       if (error == 11000) {
+//         res.status(409).json({ message: "Already in Use" });
+//       }
+//       res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-app.get("/list", async (req, res) => {
+app.get("/list", authMiddleware, async (req, res) => {
   const data = await license.find({ approved: false });
   res.render("list", { data: data });
 });
 
+//license application
+app.get("/addLicense", async (req, res) => {
+  res.render("licenseApplication");
+});
 app.post("/addLicense", async (req, res) => {
   try {
     const newApplication = new license({
@@ -62,14 +107,14 @@ app.post("/addLicense", async (req, res) => {
       proprietor: req.body.proprietor,
     });
     await license.create(newApplication);
+    res.render("success", { data: newApplication });
   } catch (e) {
     res.send(e);
   }
-  res.send("done");
 });
 
 //approval in blockchain
-app.get("/approve/:id", async (req, res) => {
+app.get("/approve/:id", authMiddleware, async (req, res) => {
   try {
     const data = await license.findById(req.params.id);
     res.render("approval", { application: data });
@@ -78,7 +123,7 @@ app.get("/approve/:id", async (req, res) => {
   }
 });
 
-app.post("/approve", async (req, res) => {
+app.post("/approve", authMiddleware, async (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
   const proprietor = req.body.proprietor;
